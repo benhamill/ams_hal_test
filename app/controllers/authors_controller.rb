9f85370b968706e6939cd3b9f0f50562c0bbb1ee
Ben Hamill <git-commits@benhamill.com>
2013-11-09 02:09:36 -0600

    Drop in a self link for authors.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,8 @@ class AuthorsController < ApplicationController
   def index
     render hal: Author.order('created_at DESC')
   end
+
+  def show
+    render hal: Author.find(params[:id])
+  end
 end
--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -1,6 +1,13 @@
 class AuthorSerializer < HalSerializer
   attributes :name, :created_at
 
+  link :self do |author_serializer|
+    {
+      href: Zooplankton.path_template_for(:author, id: author_serializer.object.id),
+      templated: true
+    }
+  end
+
   def created_at
     object.created_at.strftime("%b %d %Y")
   end
